2f941ce3d267f3cab79c8983aef7a2975b04ec13
icefluffy <icefluffy@users.noreply.github.com>
2026-06-02 16:01:11 +0200

    Refactor stock request picking type function

--- stock_request_picking_type/hooks.py
+++ stock_request_picking_type/hooks.py
@@ -1,62 +1,64 @@
 from odoo import SUPERUSER_ID, api
 
 
-def _ensure_stock_request_picking_type(env):
-    warehouse = env.ref("stock.warehouse0", raise_if_not_found=False)
+def _ensure_stock_request_picking_types(env):
     sequence = env.ref("stock_request.seq_stock_request_order", raise_if_not_found=False)
-
-    if not warehouse or not sequence:
+    if not sequence:
         return
 
-    company = warehouse.company_id
-    PickingType = env["stock.picking.type"].with_company(company)
+    warehouses = env["stock.warehouse"].search([])
 
-    picking_type = PickingType.search(
-        [
-            ("code", "=", "stock_request_order"),
-            ("warehouse_id", "=", warehouse.id),
-            ("company_id", "=", company.id),
-        ],
-        limit=1,
-    )
+    for warehouse in warehouses:
+        company = warehouse.company_id
+        PickingType = env["stock.picking.type"].with_company(company)
 
-    vals = {
-        "name": f"{warehouse.name}: Stock Requests",
-        "sequence_id": sequence.id,
-        "code": "stock_request_order",
-        "sequence_code": "SRO",
-        "warehouse_id": warehouse.id,
-        "company_id": company.id,
-    }
+        picking_type = PickingType.search(
+            [
+                ("code", "=", "stock_request_order"),
+                ("warehouse_id", "=", warehouse.id),
+                ("company_id", "=", company.id),
+            ],
+            limit=1,
+        )
 
-    if picking_type:
-        picking_type.write(vals)
-    else:
-        picking_type = PickingType.create(vals)
+        vals = {
+            "name": f"{warehouse.name}: Stock Requests",
+            "sequence_id": sequence.id,
+            "code": "stock_request_order",
+            "sequence_code": "SRO",
+            "warehouse_id": warehouse.id,
+            "company_id": company.id,
+        }
 
-    imd = env["ir.model.data"].search(
-        [
-            ("module", "=", "stock_request_picking_type"),
-            ("name", "=", "stock_request_order"),
-            ("model", "=", "stock.picking.type"),
-        ],
-        limit=1,
-    )
+        if picking_type:
+            picking_type.write(vals)
+        else:
+            picking_type = PickingType.create(vals)
 
-    imd_vals = {
-        "module": "stock_request_picking_type",
-        "name": "stock_request_order",
-        "model": "stock.picking.type",
-        "res_id": picking_type.id,
-        "noupdate": True,
-    }
+        xmlid_name = f"stock_request_order_wh_{warehouse.id}"
+        imd = env["ir.model.data"].search(
+            [
+                ("module", "=", "stock_request_picking_type"),
+                ("name", "=", xmlid_name),
+                ("model", "=", "stock.picking.type"),
+            ],
+            limit=1,
+        )
 
-    if imd:
-        imd.write(imd_vals)
-    else:
-        env["ir.model.data"].create(imd_vals)
+        imd_vals = {
+            "module": "stock_request_picking_type",
+            "name": xmlid_name,
+            "model": "stock.picking.type",
+            "res_id": picking_type.id,
+            "noupdate": True,
+        }
+
+        if imd:
+            imd.write(imd_vals)
+        else:
+            env["ir.model.data"].create(imd_vals)
 
 
 def post_init_hook(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    _ensure_stock_request_picking_type(env)
+    _ensure_stock_request_picking_types(env)
