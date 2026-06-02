3328c08dcab834192ce749c448c07e1b94761f16
icefluffy <icefluffy@users.noreply.github.com>
2026-06-02 14:58:02 +0200

    Update __manifest__.py

--- stock_request_submit/__manifest__.py
+++ stock_request_submit/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": "Add submit state on Stock Requests",
     "version": "18.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/OCA/stock-logistics-request",
+    "website": "https://github.com/icefluffy/stock-logistics-request",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock_request"],
