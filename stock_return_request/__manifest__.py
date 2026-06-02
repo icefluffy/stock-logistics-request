2310b0e309fed9e18017f2a6ef71333b6f05be88
icefluffy <icefluffy@users.noreply.github.com>
2026-06-02 14:58:39 +0200

    Update __manifest__.py

--- stock_return_request/__manifest__.py
+++ stock_return_request/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Return Request",
     "version": "18.0.1.0.1",
     "category": "Stock",
-    "website": "https://github.com/OCA/stock-logistics-request",
+    "website": "https://github.com/icefluffy/stock-logistics-request",
     "author": "Tecnativa, " "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
